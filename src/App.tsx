d5301419ec1d38567c1e9cbc8a006852b71e8524
ISHAN-py <achonam69@gmail.com>
2025-09-15 20:26:23 +0530

    v3.2.1

--- src/App.tsx
+++ src/App.tsx
@@ -117,9 +117,9 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-900 animated-bg overflow-hidden">
+    <div className="min-h-screen bg-gradient-to-br from-indigo-950 via-purple-950 to-black animated-bg overflow-hidden">
       {/* Background Pattern */}
-      <div className="absolute inset-0 opacity-10">
+      <div className="absolute inset-0 opacity-5">
         <div className="absolute top-20 left-10 w-72 h-72 bg-white rounded-full blur-3xl"></div>
         <div className="absolute bottom-20 right-10 w-96 h-96 bg-cyan-400 rounded-full blur-3xl"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-purple-400 rounded-full blur-3xl"></div>
